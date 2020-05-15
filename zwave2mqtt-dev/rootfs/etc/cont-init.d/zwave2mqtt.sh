998d3eb61aa40e2f62b6971629dd91b6ee0a023b
outkastm <irinel2k@yahoo.com>
2020-05-15 15:00:54 +0200

    initial

---
+++ zwave2mqtt-dev/rootfs/etc/cont-init.d/zwave2mqtt.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/with-contenv bashio
+# ==============================================================================
+# Home Assistant Community Add-on: Z-Wave to MQTT
+# Lorem ipsum
+# ==============================================================================
+
+# Ensure store is persistent
+rm -f -r /opt/store
+ln -s /data /opt/store
